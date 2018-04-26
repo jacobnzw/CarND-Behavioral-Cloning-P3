fb22ca41f68f39186b316af39fb3412d284bf2e6
Jakub Prüher <jacobnzw@gmail.com>
2018-04-26 13:49:03 +0200

    Simple architecture coded.

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import numpy as np
 
 # read all the lines of the csv
 lines = []
-with open('./data/joined/driving_log.csv') as csvfile:
+with open('./data/data_forw_mouse_2laps/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
@@ -15,7 +15,7 @@ for line in lines:
     # get the image path
     source_path = line[0]
     filename = source_path.split('\\')[-1]
-    current_path = './data/joined/IMG/' + filename
+    current_path = './data/data_forw_mouse_2laps/IMG/' + filename
 
     # load the image using OpenCV
     image = cv2.imread(current_path)
@@ -29,13 +29,28 @@ X_train = np.array(images)
 y_train = np.array(steering_angles)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Convolution2D, MaxPooling2D
 
+# TODO: data normalization using Lambda layers (might glitch)
+# TODO: BatchNormalization for deep nets
+# TODO: Transfer learning?
+
+conv_opt = {'border_mode': 'same', 'activation': 'relu'}
 model = Sequential()
-model.add(Flatten(input_shape=(160, 320, 3)))
+model.add(Convolution2D(4, 5, 5, input_shape=(160, 320, 3), **conv_opt))
+model.add(MaxPooling2D(pool_size=(2, 2), border_mode='same'))
+model.add(Convolution2D(8, 5, 5, **conv_opt))
+model.add(MaxPooling2D())
+model.add(Convolution2D(16, 3, 3, **conv_opt))
+model.add(MaxPooling2D(pool_size=(2, 2), border_mode='same'))
+model.add(Convolution2D(32, 3, 3, **conv_opt))
+model.add(MaxPooling2D())
+model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=10, batch_size=32)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=10, batch_size=128)
 
 model.save('model.h5')
