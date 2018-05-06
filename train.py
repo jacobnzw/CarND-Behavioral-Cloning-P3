c31452f3429022c0618536862d173cc63429053e
Jakub Prüher <jacobnzw@gmail.com>
2018-05-06 12:03:09 +0200

    Optional command line arguments added.

--- train.py
+++ train.py
@@ -1,25 +1,15 @@
 import os
 import csv
+import argparse
 import cv2
 import numpy as np
 import sklearn
 from sklearn.model_selection import train_test_split
 
-BASE_PATH = os.path.join('.', 'data')
-IMG_BASE_PATH = os.path.join(BASE_PATH, 'IMG')
-DRIVE_LOG_PATH = os.path.join(BASE_PATH, 'driving_log.csv')
-
-samples = []
-with open(os.path.join(DRIVE_LOG_PATH)) as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        samples.append(line)
-
-train_samples, validation_samples = train_test_split(samples, test_size=0.2)
-print('len(train_samples)={:d}, len(validation_samples)={:d}'.format(len(train_samples), len(validation_samples)))
+DEFAULT_BASE_PATH = os.path.join('.', 'data')
 
 
-def generator(samples, batch_size=32):
+def generator(samples, imgdir, batch_size=32):
     num_samples = len(samples)
     while 1:  # Loop forever so the generator never terminates
         sklearn.utils.shuffle(samples)
@@ -34,7 +24,7 @@ def generator(samples, batch_size=32):
                 SEP = '/' if '/' in batch_sample[0] else '\\'
 
                 # read the center image in RGB format
-                name = os.path.join(IMG_BASE_PATH, batch_sample[0].split(SEP)[-1])
+                name = os.path.join(imgdir, batch_sample[0].split(SEP)[-1])
                 center_image = cv2.cvtColor(cv2.imread(name), cv2.COLOR_BGR2RGB)
                 # read the steering angle
                 center_angle = float(batch_sample[3])
@@ -123,15 +113,49 @@ def mynet():
     return model
 
 
-optim = Adam(lr=0.001)
-model = nvidia_net()
-# model = mynet()
-model.compile(loss='mse', optimizer=optim)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-e', '--epochs', type=int, default=5)
+    parser.add_argument('-l', '--learning-rate', type=int, default=0.001)
+    parser.add_argument('-d', '--base-directory', type=str, default=DEFAULT_BASE_PATH)
+    parser.add_argument('-o', '--output-file', type=str, default='model.h5')
+    args = parser.parse_args()
 
-print('Training...')
-train_generator = generator(train_samples, batch_size=32)
-validation_generator = generator(validation_samples, batch_size=32)
-model.fit_generator(train_generator, nb_epoch=5, samples_per_epoch=2*len(train_samples),
-                    validation_data=validation_generator, nb_val_samples=2*len(validation_samples))
-model.save('model.h5')
-print('Model saved.')
+    img_directory = os.path.join(args.base_directory, 'IMG')
+    drive_log_path = os.path.join(args.base_directory, 'driving_log.csv')
+    print('Image directory: {}'.format(img_directory))
+    print('Driving log file: {}'.format(drive_log_path))
+
+    # load lines from CSV log
+    samples = []
+    with open(os.path.join(drive_log_path)) as csvfile:
+        reader = csv.reader(csvfile)
+        for line in reader:
+            samples.append(line)
+    train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+    print('# training examples: {:d}'.format(len(train_samples)))
+    print('# validation examples: {:d}'.format(len(validation_samples)))
+
+    # histogram of steering angles
+    import matplotlib.pylab as plt
+    angles = np.array([float(line[3]) for line in samples])
+    plt.title('Distribution of Steering Angles')
+    plt.hist(angles, density=True, bins=100, log=False)
+    plt.show()
+
+    # specify optimizer, compile the model
+    optim = Adam(lr=args.learning_rate)
+    model = nvidia_net()
+    # model = mynet()
+    model.compile(loss='mse', optimizer=optim)
+
+    print('Training...')
+    train_generator = generator(train_samples, img_directory, batch_size=32)
+    validation_generator = generator(validation_samples, img_directory, batch_size=32)
+    model.fit_generator(train_generator,
+                        nb_epoch=args.epochs,
+                        samples_per_epoch=2*len(train_samples),
+                        validation_data=validation_generator,
+                        nb_val_samples=2*len(validation_samples))
+    model.save(args.output_file)
+    print('Model saved.')
